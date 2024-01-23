4ae92b023edff8200bfbab4f01f740f4bc8e6acd
vinsky001 <ekibet544@gmail.com>
2024-01-23 11:46:31 +0300

    CSS changes: removed all padding and margin from the body and added box-sizing: border-box.

---
+++ keeper/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Note from './components/note';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+    </div>
+  );  
+}
+
+export default App;
