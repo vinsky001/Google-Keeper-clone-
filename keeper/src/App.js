b030b7423a49e489fcc0fca9c2a72aaf95a8faa9
vinsky001 <ekibet544@gmail.com>
2024-01-26 17:31:32 +0300

    Update note component to accept title and content as props

--- keeper/src/App.js
+++ keeper/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Note from './components/note';
+import notes from './notes';
 
-
+function createNotes(noteEntry) {
+  return <Note
+  key={noteEntry.key}
+  title={noteEntry.title}
+  content={noteEntry.content}
+  />
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
-    </div>
+      {notes.map(createNotes)}
+      {/* <Note title="Note title"  content="Note contents"   /> */}
+       </div>
   );  
 }
 
