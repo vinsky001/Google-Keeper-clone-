ab57348e82337fc04188a2acd7aba43b8bfda9b4
vinsky001 <ekibet544@gmail.com>
2024-01-27 19:48:30 +0300

    Simplified code to ES 6 js

--- keeper/src/App.js
+++ keeper/src/App.js
@@ -3,18 +3,18 @@ import Header from './components/Header';
 import Note from './components/note';
 import notes from './notes';
 
-function createNotes(noteEntry) {
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(noteEntry => {
   return <Note
   key={noteEntry.key}
   title={noteEntry.title}
   content={noteEntry.content}
   />
-}
-function App() {
-  return (
-    <div>
-      <Header />
-      {notes.map(createNotes)}
+})}
       {/* <Note title="Note title"  content="Note contents"   /> */}
        </div>
   );  
